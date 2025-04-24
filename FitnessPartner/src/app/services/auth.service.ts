c103b0fa77854519c9d581b5273318495c094951
Omar9Tarek <Omar9Tarek@gmail.com>
2025-04-24 04:14:20 +0200

    introduced backend

--- FitnessPartner/src/app/services/auth.service.ts
+++ FitnessPartner/src/app/services/auth.service.ts
@@ -1,6 +1,35 @@
 
+// import { Injectable } from '@angular/core';
+// import { BehaviorSubject } from 'rxjs';
+
+// @Injectable({
+//   providedIn: 'root'
+// })
+// export class AuthService {
+//   private isLoggedInSubject = new BehaviorSubject<boolean>(!!localStorage.getItem('token'));
+//   isLoggedIn$ = this.isLoggedInSubject.asObservable();
+
+//   login(token: string) {
+//     localStorage.setItem('token', token);
+//     this.isLoggedInSubject.next(true);
+//   }
+
+//   logout() {
+//     localStorage.removeItem('token');
+//     this.isLoggedInSubject.next(false);
+//   }
+
+//   getCurrentUserId(): string | null {
+//     const token = localStorage.getItem('token');
+//     return token ? atob(token) : null;
+//   }
+// }
+
+////////////////// TESTING BACKEND BELOW //////////////////////// ORIGINAL CODE ABOVE //// DO NOT DELETE ///////
+// auth.service.ts in Angular
 import { Injectable } from '@angular/core';
-import { BehaviorSubject } from 'rxjs';
+import { HttpClient } from '@angular/common/http';
+import { BehaviorSubject, Observable, tap } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
@@ -8,19 +37,41 @@ import { BehaviorSubject } from 'rxjs';
 export class AuthService {
   private isLoggedInSubject = new BehaviorSubject<boolean>(!!localStorage.getItem('token'));
   isLoggedIn$ = this.isLoggedInSubject.asObservable();
+  private apiUrl = 'http://localhost:3000/auth';
 
-  login(token: string) {
-    localStorage.setItem('token', token);
-    this.isLoggedInSubject.next(true);
+  constructor(private http: HttpClient) {}
+
+  login(credentials: { email: string; password: string }): Observable<any> {
+    return this.http.post(`${this.apiUrl}/login`, credentials).pipe(
+      tap((response: any) => {
+        localStorage.setItem('token', response.access_token);
+        localStorage.setItem('userId', response.user.id);
+        this.isLoggedInSubject.next(true);
+      })
+    );
+  }
+
+  register(userData: any): Observable<any> {
+    return this.http.post(`${this.apiUrl}/register`, userData).pipe(
+      tap((response: any) => {
+        localStorage.setItem('token', response.access_token);
+        localStorage.setItem('userId', response.user.id);
+        this.isLoggedInSubject.next(true);
+      })
+    );
   }
 
   logout() {
     localStorage.removeItem('token');
+    localStorage.removeItem('userId');
     this.isLoggedInSubject.next(false);
   }
 
   getCurrentUserId(): string | null {
-    const token = localStorage.getItem('token');
-    return token ? atob(token) : null;
+    return localStorage.getItem('userId');
+  }
+
+  getToken(): string | null {
+    return localStorage.getItem('token');
   }
 }
--- FitnessPartner/src/app/services/users.service.ts
+++ FitnessPartner/src/app/services/users.service.ts
@@ -1,26 +1,61 @@
+// import { HttpClient } from '@angular/common/http';
+// import { Injectable } from '@angular/core';
+
+// @Injectable({
+//   providedIn: 'root',
+// })
+// export class UsersService {
+//   constructor(private myClient: HttpClient) {}
+//   private URLData = 'http://localhost:3000/users';
+
+//   getAllUsers() {
+//     return this.myClient.get<any>(this.URLData);
+//   }
+//   addANewUser(newUser: any) {
+//     return this.myClient.post(this.URLData, newUser);
+//   }
+//   getUserById(id: any) {
+//     return this.myClient.get(`${this.URLData}/${id}`);
+//   }
+//   deleteUser(id: any) {
+//     return this.myClient.delete(`${this.URLData}/${id}`);
+//   }
+//   editUserData(id: any, updatedUser: any) {
+//     return this.myClient.put(`${this.URLData}/${id}`, updatedUser);
+//   }
+// }
+
+////////////////// TESTING BACKEND BELOW //////////////////////// ORIGINAL CODE ABOVE //// DO NOT DELETE ///////
+
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { User } from '../shared/utils/user';
 
 @Injectable({
   providedIn: 'root',
 })
 export class UsersService {
-  constructor(private myClient: HttpClient) {}
-  private URLData = 'http://localhost:3000/users';
+  constructor(private http: HttpClient) {}
+  private baseUrl = 'http://localhost:3000';
 
-  getAllUsers() {
-    return this.myClient.get<any>(this.URLData);
+  getAllUsers(): Observable<User[]> {
+    return this.http.get<User[]>(`${this.baseUrl}/users`);
   }
-  addANewUser(newUser: any) {
-    return this.myClient.post(this.URLData, newUser);
+
+  addANewUser(newUser: User): Observable<User> {
+    return this.http.post<User>(`${this.baseUrl}/auth/register`, newUser);
   }
-  getUserById(id: any) {
-    return this.myClient.get(`${this.URLData}/${id}`);
+
+  getUserById(id: string): Observable<User> {
+    return this.http.get<User>(`${this.baseUrl}/users/${id}`);
   }
-  deleteUser(id: any) {
-    return this.myClient.delete(`${this.URLData}/${id}`);
+
+  deleteUser(id: string): Observable<User> {
+    return this.http.delete<User>(`${this.baseUrl}/users/${id}`);
   }
-  editUserData(id: any, updatedUser: any) {
-    return this.myClient.put(`${this.URLData}/${id}`, updatedUser);
+
+  editUserData(id: string, updatedUser: User): Observable<User> {
+    return this.http.put<User>(`${this.baseUrl}/users/${id}`, updatedUser);
   }
 }
