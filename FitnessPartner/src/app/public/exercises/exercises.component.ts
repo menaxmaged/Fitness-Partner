97f9d0dd029021e815872701a305f4afcb00643b
VerinaMaged <101900193+VerinaMaged@users.noreply.github.com>
2025-05-07 13:55:48 +0300

    edit workout program

--- FitnessPartner/src/app/public/exercises/exercises.component.ts
+++ FitnessPartner/src/app/public/exercises/exercises.component.ts
@@ -4,10 +4,9 @@ import { NavigationCancel, NavigationEnd, NavigationError, NavigationStart, Rout
 import { LoadingSpinnerComponent } from "../../shared/loading-spinner/loading-spinner.component";
 import { IdentifyMachineComponent } from "../../identify-machine/identify-machine.component";
 import { TranslateModule, TranslateService } from '@ngx-translate/core';
-import { WorkoutsComponent } from "../../workout-list/workout-list.component";
 @Component({
   selector: 'app-exercises',
-  imports: [CommonModule, LoadingSpinnerComponent, RouterLink, TranslateModule, IdentifyMachineComponent, WorkoutsComponent],
+  imports: [CommonModule, LoadingSpinnerComponent, RouterLink, TranslateModule, IdentifyMachineComponent],
   templateUrl: './exercises.component.html',
   styles: ``
 })
