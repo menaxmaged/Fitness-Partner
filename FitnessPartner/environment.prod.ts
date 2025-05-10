953de5f4dd366db2aa748529703adbabf4d4d5f0
Mena Maged <menaxmaged@gmail.com>
2025-05-10 06:07:11 +0300

    ..

--- FitnessPartner/environment.prod.ts
+++ FitnessPartner/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  apiUrl: 'https://fitness-partner-no76.vercel.app' // Replace with your actual backend URL
+  apiUrl: 'https://fitness-partner-back.vercel.app' // Replace with your actual backend URL
 };
